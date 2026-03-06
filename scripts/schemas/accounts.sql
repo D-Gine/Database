8a65906b41ed38d0471dea92627b22918f206fe9
baptiste <baptiste.dunes@epitech.eu>
2026-03-06 11:13:51 +0100

    feat(accounts): users images are now simple strings, paths of images related to api host

--- scripts/schemas/accounts.sql
+++ scripts/schemas/accounts.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS accounts;
 
 CREATE TABLE IF NOT EXISTS accounts.users (
     id          UUID            DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
-    image       UUID            DEFAULT uuid_generate_v4() NOT NULL UNIQUE,
+    image       VARCHAR(255)    UNIQUE,
     email       VARCHAR(255)    NOT NULL UNIQUE,
     name        VARCHAR(255)    NOT NULL,
     password    VARCHAR(255)    NOT NULL,
