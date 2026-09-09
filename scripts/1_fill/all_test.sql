4d2d04c405b9ba7feeee9f7b67a34841174d337c
abeldaverio <abel.daverio@epitech.eu>
2026-09-09 13:03:22 +0200

    feat: nodes and next scripts

--- scripts/1_fill/all_test.sql
+++ scripts/1_fill/all_test.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict codaCu4PkD5NcakYEP8KKmWHizpy87XD6lqyCJDc5bMcEgRdEani60qYEiL672X
+\restrict NJFa9UgBCV5I2DxqUk8bfM32z1k9Uyqf1el9eMylSzS7Dm7jHycviibaYyOpbcg
 
 -- Dumped from database version 16.15
 -- Dumped by pg_dump version 16.14
 
--- Started on 2026-09-09 10:10:36 UTC
+-- Started on 2026-09-09 10:58:32 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -21,38 +21,19 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3506 (class 0 OID 16492)
--- Dependencies: 230
--- Data for Name: link_enums_tags; Type: TABLE DATA; Schema: public; Owner: prout
+-- TOC entry 3506 (class 0 OID 16436)
+-- Dependencies: 222
+-- Data for Name: first_nodes; Type: TABLE DATA; Schema: public; Owner: prout
 --
 
-INSERT INTO public.link_enums_tags VALUES ('e87084a9-f3ba-40aa-ac4a-501fd27facb8', '0e8ef112-a906-4c50-8c68-26501212eaa0');
-INSERT INTO public.link_enums_tags VALUES ('8dc8d9aa-98b6-475c-a5fb-c377f888c888', '0e8ef112-a906-4c50-8c68-26501212eaa0');
-INSERT INTO public.link_enums_tags VALUES ('5c93ce27-c2d0-4924-916c-abefabc0ccfd', '64f73b11-5699-46a2-9f63-4db41f18e472');
-INSERT INTO public.link_enums_tags VALUES ('33d4255f-f405-4bb5-add5-f85e0ff43e5b', '64f73b11-5699-46a2-9f63-4db41f18e472');
-INSERT INTO public.link_enums_tags VALUES ('a87fa8f3-d56a-49ec-8bd2-d62b7f07c367', '64f73b11-5699-46a2-9f63-4db41f18e472');
-INSERT INTO public.link_enums_tags VALUES ('3f9eb017-4106-416d-8158-78ceacb79fe3', '5b928a28-319f-4da2-b4e3-96346c037dbf');
-INSERT INTO public.link_enums_tags VALUES ('d7d7ece5-069d-432a-9ce2-39454b04eef9', '5b928a28-319f-4da2-b4e3-96346c037dbf');
-INSERT INTO public.link_enums_tags VALUES ('2d2261d6-a046-4f81-b20a-dcdb14f76b40', '5b928a28-319f-4da2-b4e3-96346c037dbf');
-INSERT INTO public.link_enums_tags VALUES ('2b9e8f09-12c1-459b-b1be-97af146be0f3', '5b928a28-319f-4da2-b4e3-96346c037dbf');
-INSERT INTO public.link_enums_tags VALUES ('d3b77571-8346-4424-8e91-2d37b788d2c9', 'f123bb0d-7dcc-492a-884e-90312d2d7bbf');
-INSERT INTO public.link_enums_tags VALUES ('69b6ff7d-e69e-4525-b952-a4155bfb22b1', 'f123bb0d-7dcc-492a-884e-90312d2d7bbf');
-INSERT INTO public.link_enums_tags VALUES ('69b6ff7d-e69e-4525-b952-a4155bfb22b1', '96b2b0d9-71d0-4611-8da2-35ca07694db8');
-INSERT INTO public.link_enums_tags VALUES ('d3b77571-8346-4424-8e91-2d37b788d2c9', '96b2b0d9-71d0-4611-8da2-35ca07694db8');
-INSERT INTO public.link_enums_tags VALUES ('2d2261d6-a046-4f81-b20a-dcdb14f76b40', 'a0f4a684-a490-4652-9c85-fd68b9e16ad8');
-INSERT INTO public.link_enums_tags VALUES ('3f9eb017-4106-416d-8158-78ceacb79fe3', 'a0f4a684-a490-4652-9c85-fd68b9e16ad8');
-INSERT INTO public.link_enums_tags VALUES ('d7d7ece5-069d-432a-9ce2-39454b04eef9', 'b335bf1c-1682-44f3-bf35-c4129cea466f');
-INSERT INTO public.link_enums_tags VALUES ('2b9e8f09-12c1-459b-b1be-97af146be0f3', 'b335bf1c-1682-44f3-bf35-c4129cea466f');
-INSERT INTO public.link_enums_tags VALUES ('33d4255f-f405-4bb5-add5-f85e0ff43e5b', '96b2b0d9-71d0-4611-8da2-35ca07694db8');
-INSERT INTO public.link_enums_tags VALUES ('a87fa8f3-d56a-49ec-8bd2-d62b7f07c367', 'a0f4a684-a490-4652-9c85-fd68b9e16ad8');
-INSERT INTO public.link_enums_tags VALUES ('5c93ce27-c2d0-4924-916c-abefabc0ccfd', 'b335bf1c-1682-44f3-bf35-c4129cea466f');
+INSERT INTO public.first_nodes VALUES ('d618c90b-7dfe-4cdb-9e5d-a418d747b590', '0e3196a0-f8d8-444f-a6b6-d5f6f81f6333');
 
 
--- Completed on 2026-09-09 10:10:36 UTC
+-- Completed on 2026-09-09 10:58:32 UTC
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict codaCu4PkD5NcakYEP8KKmWHizpy87XD6lqyCJDc5bMcEgRdEani60qYEiL672X
+\unrestrict NJFa9UgBCV5I2DxqUk8bfM32z1k9Uyqf1el9eMylSzS7Dm7jHycviibaYyOpbcg
 
